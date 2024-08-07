831fde1d7e5cabff8229d4fc1bf2b3c40e58fdb2
Belol <belal.ahmady@gmx.de>
2024-08-07 23:30:45 +0200

    ...

--- frontend/src/components/TableLabel.jsx
+++ frontend/src/components/TableLabel.jsx
@@ -5,11 +5,11 @@ export default function TableLabel() {
   return (
     
     <div className=" text-lg font-medium text-white w-full flex basis-full justify-between border-b border-t-[1px] " >
-            <p className="px-2 py-3 min-w-10">Uhrzeit</p>
-            <p className="px-4 py-3 min-w-10">Wetter</p>
-            <p className="px-2 py-3 min-w-10">Temp.(°C)</p>
-            <p className="px-2 pt-2 min-w-10 text-2xl mr-2">🌧️</p>
-            <p className="px-2 pt-2 min-w-10 text-2xl">🌨️</p>
+            <p className="pl-2 py-3 min-w-10">Uhrzeit</p>
+            <p className=" py-3 min-w-10">Wetter</p>
+            <p className=" py-3 min-w-10">Temp.(°C)</p>
+            <p className=" pr-2 pt-2 min-w-10 text-2xl">🌧️</p>
+            <p className=" pt-2 min-w-10 text-2xl">🌨️</p>
     </div>
   )
 }
