fd41d253e12f15c909deb4592b666a07dc1de8d3
Belol <belal.ahmady@gmx.de>
2024-08-07 23:22:42 +0200

    adjusting some font-sizes

--- frontend/src/components/DateButton.jsx
+++ frontend/src/components/DateButton.jsx
@@ -7,7 +7,7 @@ export default function DateButton({firstPart, dayAndMonths, handleSetWeatherDay
   return (
     <button
         onClick={() => setWeatherDay(index)}
-        className={`${weatherDayIndex === index && " bg-gradient-to-r from-slate-300 to-slate-500 min-w-10 z-10 shadow-sm shadow-white text-xl"} border-black border-t-2 flex cursor-pointer min-w-8 flex-col basis-[30%] items-center rounded-xl bg-gray-100 text-lg hover:text-xl `}>
+        className={`${weatherDayIndex === index && " bg-gradient-to-r from-slate-300 to-slate-500 min-w-10 z-10 shadow-sm shadow-white"} border-black border-t-2 flex cursor-pointer min-w-8 flex-col basis-[30%] items-center rounded-xl bg-gray-100 text-base hover:text-xl `}>
       <p className='font-bold  basis-auto mb-0 '>{firstPart}</p>
       <p className='mt-0 w-max basis-auto'>{dayAndMonths}</p>
     </button>
